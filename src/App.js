2c5291c8551d60f80fa3bc4bb80fdef2717a1399
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-09-12 17:54:19 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Team 3 Members: Tyler Quinn 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
