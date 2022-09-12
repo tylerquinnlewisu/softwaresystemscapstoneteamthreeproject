85973921b58a2c611617df82ee96e5297d226cb1
Kyle Bye <kylebye4mav@gmail.com>
2022-09-12 18:22:16 -0500

    added name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Team 3 Members: Tyler Quinn 
+          Team 3 Members: Tyler Quinn, Kyle Bye
         </p>
       </header>
     </div>
