025b1b2e25fa447c6fc791f2e87c3e0dc17b323b
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-10-18 13:18:31 -0500

    Added Jotform

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import './App.css';
 
+const dono = `<iframe
+id="JotFormIFrame-222896978943177"
+title="Online Donation Form"
+onload="window.parent.scrollTo(0,0)"
+allowtransparency="true"
+allowfullscreen="true"
+allow="geolocation; microphone; camera"
+src="https://form.jotform.com/222896978943177"
+frameborder="0"
+style="
+position: relative;
+right: 190px;
+bottom: 195px;
+min-width: 245%;
+height:1625px;
+border:none;"
+scrolling="no"
+background: transparent !important;
+>
+</iframe>
+`; 
 
 function App() {
   return (
@@ -12,6 +33,11 @@ function App() {
           Team 3 Members: Tyler Quinn, Kyle Bye, Antonio Pantoja, Matthew Beaulieu
         </p>
       </header>
+      <body>
+        <div dangerouslySetInnerHTML={{__html:dono}}>
+        </div>
+
+      </body>
     </div>
   );
 }
