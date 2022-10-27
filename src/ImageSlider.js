cc86b9d53cd87a245b5f3c1a042512a9916c4ba1
matthew <matthewbeaulieu10@gmail.com>
2022-10-26 21:40:49 -0500

    got rid of unnecesarry variables

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,10 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { SliderData } from './SliderData';
 
 const ImageSlider = () => {
-const [current, setCurrent] = useState(0)
-
-
     return (
         <> 
         {SliderData.map((slide, index) => {
