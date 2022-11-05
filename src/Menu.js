4f3c464392ec4486e0c7534fd4b5492196496402
Tyler Quinn <adminaccountrun@Tylers-MacBook-Pro-3.local>
2022-11-04 19:09:43 -0500

    Menu chang

--- src/Menu.js
+++ src/Menu.js
@@ -5,10 +5,10 @@ export default function Menu () {
     return (
     <div class="menuSection">
         <ul class="menu">
-          <li class="link"><a class="link_a" href="/">Home</a></li>
-          <li class="link"><a class="link_a" href="/donate">Donate</a></li>
-          <li class="link"><a class="link_a" href="/slideshow">Slideshow</a></li>
-          <li class="link"><a class="link_a"  href="/about">About</a></li>
+          <li class="link"><a class="link_a" href="./">Home</a></li>
+          <li class="link"><a class="link_a" href="./donate">Donate</a></li>
+          <li class="link"><a class="link_a" href="./slideshow">Slideshow</a></li>
+          <li class="link"><a class="link_a"  href="./about">About</a></li>
         </ul>
         <a id ="login" href="/login" >Login</a>  
         <a id ="register" href="/register">Register</a>      
